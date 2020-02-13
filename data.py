9ede54bed0c603f8817e3ab4d3c79368ab674482
Xinyang <liuxyxiang@gmail.com>
2020-02-12 20:57:53 -0500

    some bug fix, loggaussian

--- data.py
+++ data.py
@@ -2,30 +2,36 @@
 import numpy as np
 from scipy.stats import multivariate_normal as multigauss
 
-prior_dim = 1
-prior_mean = 22000
-prior_sigma = 2000
-obs_values = np.array([12.84, 13.12, 12.13, 12.19, 12.67])/1000
-N_prior_samples = 30000
-N_resample = 10000
 
 class data:
     '''
-    mean, sigma, number of samples, array of sample values
+    number of samples, array of sample values
     '''
-    def __init__(self, dim, mean, sigma, size):
+    def __init__(self, dim, size):
         self.dim = dim
+        self.size = size
+        self.sample = np.zeros(size)
+        self.pdfvalue = np.zeros(size)
+    
+
+class gaussiandata(data):
+    '''
+    for Gaussian Distribution
+
+    '''
+    def __init__(self, dim, size, mean, sigma):
+        super().__init__(dim, size)
         self.mean = mean
         self.sigma = sigma
-        self.size = size
-        self.sample = np.random.normal(loc=self.mean, scale=self.sigma, size=self.size)
-    
+
     def pdf(self):
         '''
         probability density function
         '''
-        return multigauss.pdf(self.sample, mean=self.mean, cov=self.sigma)
+        self.pdfvalue = multigauss.pdf(self.sample, mean=self.mean, cov=self.sigma)
+        return self.pdfvalue
 
-def initprior():
-    prior = data(1, prior_mean, prior_sigma, N_prior_samples)
-    return prior, obs_values, N_resample
+    def logpdf(self):
+        return multigauss.logpdf(self.sample, mean=self.mean, cov=self.sigma)
+
+
--- main1.py
+++ main1.py
@@ -4,35 +4,46 @@ from scipy.stats import multivariate_normal as multigauss
 import data
 from forward import forward
 from resample import resample
+################################################################################
 
 
-prior, obs_values, N_resample = data.initprior()
-
-likelihood = np.ones(prior.size)
-sim_sample = forward(prior.sample)
-
-#for observation at same point more than once
-for j in range(prior.size):
-    for i in range(len(obs_values)):
-        likelihood[j] *= multigauss.pdf(obs_values[i],
-                                    mean=sim_sample[j], cov=1e-6)
+np.random.seed(1)
+prior_dim = 1
+prior_mean = 22000
+prior_sigma = 2000
+obs_values = np.array([13.1])/1000
+N_prior_samples = 30000
+N_resample = 10000
 
 
-normalization = likelihood.sum()/prior.size
-posterior = likelihood*multigauss.pdf(prior.sample, mean=prior.mean, cov=prior.sigma)/normalization
+prior = data.gaussiandata(prior_dim, N_prior_samples, prior_mean, prior_sigma)
+prior.sample = np.random.normal(loc=prior.mean, scale=prior.sigma,size=prior.size)
+likelihood, sim = data.data(1,prior.size), data.data(1,prior.size)
+posterior = data.data(1,N_resample)
+
+sim.sample = forward(prior.sample)
 
 
-posterior_sample = resample(prior.sample, posterior, size = N_resample)
-sim_resample = forward(posterior_sample)
-mean_predict = np.mean(sim_resample)
-var_predict = prior.size/(prior.size -1) * np.var(sim_resample)
-print(posterior_sample.mean(), prior.sample.mean())
+for i in range(prior.size):
+    likelihood.pdfvalue[i] = multigauss.logpdf(obs_values, 
+                                mean=sim.sample[i], cov=1e-6).sum()
+
+
+#normalization = likelihood.pdfvalue.sum()/prior.size
+posterior.pdfvalue = likelihood.pdfvalue + prior.logpdf()#/normalization
+
+
+posterior.sample = resample(prior.sample, posterior.pdfvalue, size = N_resample)
+sim.sample = forward(posterior.sample)
+mean_predict = np.mean(sim.sample)
+var_predict = prior.size/(prior.size -1) * np.var(sim.sample)
+print(posterior.sample.mean(), prior.sample.mean())
 
 
 fig, ax = plt.subplots(1, 3)
 ax[0].hist(prior.sample,20)
-ax[1].hist(sim_resample,50)
+ax[2].hist(sim.sample,20)
 #ax[1].plot(obs_values)
-ax[2].hist(posterior_sample,20)
+ax[1].hist(posterior.sample,20)
 fig.tight_layout()
 plt.show()
